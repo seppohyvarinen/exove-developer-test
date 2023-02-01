848aceb946c2c2c8ca3cfdd44d4fe0bcf03c11ca
seppohyvarinen <seppo.hyvarinen@tuni.fi>
2023-02-01 16:15:44 +0200

    Add: images and imagesources to carousel items

--- section_2/2a/src/App.tsx
+++ section_2/2a/src/App.tsx
@@ -4,6 +4,20 @@ import { Carousel } from "./components/Carousel";
 import "./App.css";
 
 function App() {
+  const carouselItems = [
+    {
+      imageSrc: "./images/smiley1.png",
+      imageAlt: "A regular smiley",
+    },
+    {
+      imageSrc: "./images/smiley2.png",
+      imageAlt: "A strange smiley",
+    },
+    {
+      imageSrc: "./images/smiley3.png",
+      imageAlt: "Smiley that says I don't know",
+    },
+  ];
   return (
     <div className="mainContainer">
       <Carousel></Carousel>
