743a710b150575ebcce62c11e4a80725693744f8
seppohyvarinen <seppo.hyvarinen@tuni.fi>
2023-02-01 16:06:54 +0200

    Add: initialize carousel component

--- section_2/2a/src/components/Carousel.tsx
+++ section_2/2a/src/components/Carousel.tsx
@@ -1,3 +1,25 @@
+import { useState } from "react";
+
 export const Carousel: React.FC<{}> = () => {
-  return <></>;
+  const [index, setIndex] = useState<number>(0);
+  const length = 3;
+
+  const handlePrevious = () => {
+    const newIndex = index - 1;
+    setIndex(newIndex < 0 ? length - 1 : newIndex);
+  };
+
+  const handleNext = () => {
+    const newIndex = index + 1;
+    setIndex(newIndex >= length ? 0 : newIndex);
+  };
+
+  return (
+    <div className="carousel">
+            <button onClick={handlePrevious}>Previous</button>
+            <button onClick={handleNext}>Next</button>
+            <p>{index}</p>
+          
+    </div>
+  );
 };
