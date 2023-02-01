0cbe44bfdf97f43626f20e28091f86679dd8222b
seppohyvarinen <seppo.hyvarinen@tuni.fi>
2023-02-01 12:29:07 +0200

    Add: 3a sql table created succesfully

--- section_3/3a/database.js
+++ section_3/3a/database.js
@@ -1,5 +1,38 @@
-require("dotenv").config();
-const mysql = require("mysql");
-const Validator = require("jsonschema").Validator;
+import dotenv from "dotenv";
+dotenv.config();
+import mysql from "mysql";
 
-const validator = new Validator();
+let config = {
+  connectionLimit: 10,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DB,
+};
+let pool = mysql.createPool(config);
+
+pool.on("acquire", function (connection) {
+  console.log("***");
+  console.log("Connection %d acquired", connection.threadId);
+});
+
+pool.on("release", function (connection) {
+  console.log("Connection %d released", connection.threadId);
+});
+
+let dbFunctions = {
+  createTable: () =>
+    new Promise((resolve, reject) => {
+      let sql =
+        "create table if not exists `exove-products`(`id` varchar(40), `name` varchar(30), `description_en` varchar(250), `description_fi` varchar(250), `description_es` varchar(250), `price` decimal(8,2), `categories` varchar(200))";
+      pool.query(sql, (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve("Table created succesfully");
+        }
+      });
+    }),
+};
+
+export default dbFunctions;
--- section_3/3a/index.js
+++ section_3/3a/index.js
@@ -1,13 +1,22 @@
 import fetch from "node-fetch";
+import dbFunctions from "./database.js";
 
 async function getData() {
-  let response = await fetch(
-    "https://raw.githubusercontent.com/Exove/developer-test/main/material/products.json"
-  );
+  try {
+    let response = await fetch(
+      "https://raw.githubusercontent.com/Exove/developer-test/main/material/products.json"
+    );
 
-  let jsoned = await response.json();
+    let jsoned = await response.json();
 
-  console.log(jsoned);
+    console.log(jsoned);
+
+    let create = await dbFunctions.createTable();
+
+    console.log(create);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 getData();
